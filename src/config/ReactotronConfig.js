31d946db94cede286cf5cf6ab9f733b6ce8ec75b
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-15 19:21:36 -0400

    adds redux integration with reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
-if (__DEV__) {
+if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .useReactNative()
+    .use(reactotronRedux())
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
