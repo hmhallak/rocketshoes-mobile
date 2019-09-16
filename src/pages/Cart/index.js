41199ae8a112e6c59c400b0a7209f332354ded3a
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-15 21:48:23 -0400

    adds product listing on cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,106 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { CartContainer, EmptyText } from './styles';
 
-export default function Cart() {
+import { formatPrice } from '../../util/format';
+
+import {
+  Container,
+  Products,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductDetails,
+  ProductPrice,
+  ProductInfo,
+  EmptyContainer,
+  EmptyText,
+  ProductDelete,
+  ProductControls,
+  ProductControlButton,
+  ProductAmount,
+  ProductSubtotal,
+  TotalContainer,
+  TotalText,
+  TotalAmount,
+  Order,
+  OrderText,
+} from './styles';
+import colors from '../../styles/colors';
+
+function Cart({ navigation, products, cartSize }) {
   return (
-    <CartContainer>
-      <Icon name="remove-shopping-cart" size={60} color="#eee" />
-      <EmptyText>Seu carrinho está vazio.</EmptyText>
-    </CartContainer>
+    <Container>
+      {products.length ? (
+        <>
+          <Products>
+            {products.map(product => (
+              <Product key={product.id}>
+                <ProductInfo>
+                  <ProductImage source={{ uri: product.image }} />
+                  <ProductDetails>
+                    <ProductTitle>{product.title}</ProductTitle>
+                    <ProductPrice>{product.priceFormatted}</ProductPrice>
+                  </ProductDetails>
+                  <ProductDelete onPress={() => {}}>
+                    <Icon
+                      name="delete-forever"
+                      size={24}
+                      color={colors.primary}
+                    />
+                  </ProductDelete>
+                </ProductInfo>
+                <ProductControls>
+                  <ProductControlButton>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductAmount value={String(product.amount)} />
+                  <ProductControlButton>
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color={colors.primary}
+                    />
+                  </ProductControlButton>
+                  <ProductSubtotal>{product.subtotal}</ProductSubtotal>
+                </ProductControls>
+              </Product>
+            ))}
+            <TotalContainer>
+              <TotalText>TOTAL</TotalText>
+              <TotalAmount>R$199,90</TotalAmount>
+              <Order>
+                <OrderText>FINALIZAR PEDIDO</OrderText>
+              </Order>
+            </TotalContainer>
+          </Products>
+        </>
+      ) : (
+        <EmptyContainer>
+          <Icon name="remove-shopping-cart" size={60} color="#eee" />
+          <EmptyText>Seu carrinho está vazio.</EmptyText>
+        </EmptyContainer>
+      )}
+    </Container>
   );
 }
+
+// pega informações do estado e mapeia em forma de propriedades para o componente
+const mapStateToProps = state => ({
+  products: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
+});
+
+export default connect(mapStateToProps)(Cart);
